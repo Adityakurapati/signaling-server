5d1925023130bc49166d7f6157c237a14355d5d7
Aditya Kurapati <kurapatiaditya14@gmail.com>
2026-01-02 20:12:33 +0530

    server-2

--- singnaling-server.js
+++ singnaling-server.js
@@ -239,7 +239,6 @@ io.on('connection', (socket) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Signaling server running on port ${PORT}`);
 });
